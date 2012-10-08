8379afad20b7762a99d900c403bd81800aa9eded
Alexander Koval <kovalidis@gmail.com>
2012-10-08 14:27:52 +0300

    Bumps version to 0.2.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "solar",
-    version = "0.2.3",
+    version = "0.2.4",
     author = "Alexander Koval",
     author_email = "kovalidis@gmail.com",
     description = ("A library to use solr in python projects."),
