4e8c29274e9b4283cce1bcdfc7721ec354803cb9
Alexander Koval <kovalidis@gmail.com>
2012-12-05 15:13:58 +0200

    Updated pysolr: https://github.com/toastdriven/pysolr/blob/e0d41e3c2b3cbeaf80ca7422d89521920302f1e1/pysolr.py

--- solar/pysolr.py
+++ solar/pysolr.py
@@ -307,23 +307,26 @@ class Solr(object):
             http = Http(timeout=self.timeout)
             url = self.base_url + path
 
+            self.log.debug("Starting request to '%s' (%s) with body '%s'...",
+                           url, method, str(body)[:10])
+            start_time = time.time()
+
             try:
-                start_time = time.time()
-                self.log.debug("Starting request to '%s' (%s) with body '%s'...",
-                               url, method, str(body)[:10])
                 headers, response = http.request(url, method=method, body=body, headers=headers)
-                end_time = time.time()
-                self.log.info("Finished '%s' (%s) with body '%s' in %0.3f seconds.",
-                              url, method, str(body)[:10], end_time - start_time)
-            except AttributeError:
+            except (IOError, AttributeError):
                 error_message = "Failed to connect to server at '%s'. Are you sure '%s' is correct? Checking it in a browser might help..."
                 params = (url, self.base_url)
-                self.log.error(error_message, *params)
+                self.log.error(error_message, *params, exc_info=True)
                 raise SolrError(error_message % params)
+
+            end_time = time.time()
+            self.log.info("Finished '%s' (%s) with body '%s' in %0.3f seconds.",
+                          url, method, str(body)[:10], end_time - start_time)
 
             if int(headers['status']) != 200:
                 error_message = self._extract_error(headers, response)
-                self.log.error(error_message)
+                self.log.error(error_message, extra={'data': {'headers': headers,
+                                                              'response': response}})
                 raise SolrError(error_message)
 
             return response
@@ -339,15 +342,26 @@ class Solr(object):
             start_time = time.time()
             self.log.debug("Starting request to '%s:%s/%s' (%s) with body '%s'...",
                            self.host, self.port, path, method, str(body)[:10])
-            conn.request(method, path, body, headers)
+
+            try:
+                conn.request(method, path, body, headers)
+            except IOError:
+                error_message = "Failed to connect to server at '%s'. Are you sure '%s' is correct? Checking it in a browser might help..."
+                params = (path, self.base_url)
+                self.log.error(error_message, *params, exc_info=True)
+                raise SolrError(error_message % params)
+
             response = conn.getresponse()
             end_time = time.time()
             self.log.info("Finished '%s:%s/%s' (%s) with body '%s' in %0.3f seconds.",
                           self.host, self.port, path, method, str(body)[:10], end_time - start_time)
 
             if response.status != 200:
-                error_message = self._extract_error(dict(response.getheaders()), response.read())
-                self.log.error(error_message)
+                resp_headers = dict(response.getheaders())
+                resp_body = response.read()
+                error_message = self._extract_error(resp_headers, resp_body)
+                self.log.error(error_message, extra={'data': {'headers': resp_headers,
+                                                              'response': resp_body}})
                 raise SolrError(error_message)
 
             return response.read()
@@ -472,6 +486,8 @@ class Solr(object):
                 # html page might be different for every server
                 if server_type == 'jetty':
                     reason_node = dom_tree.find('body/pre')
+                else:
+                    reason_node = dom_tree.find('head/title')
 
                 if reason_node is not None:
                     reason = reason_node.text
@@ -796,14 +812,14 @@ class Solr(object):
                                       "".join(body_generator), headers)
         except (IOError, SolrError),  e:
             self.log.error("Failed to extract document metadata: %s", e,
-                           exc_info=e)
+                           exc_info=True)
             raise
 
         try:
             data = json.loads(resp)
         except ValueError, e:
             self.log.error("Failed to load JSON response: %s", e,
-                           exc_info=e)
+                           exc_info=True)
             raise
 
         data['contents'] = data.pop(file_obj.name, None)
