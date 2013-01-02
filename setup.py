0bf250a8bae0a28e7520c89815649b7ab218bb68
Alexander Koval <kovalidis@gmail.com>
2013-01-02 18:41:05 +0200

    Bumps version to 0.2.15

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requires():
 
 setup(
     name = "solar",
-    version = "0.2.14",
+    version = "0.2.15",
     requires = requires(),
     author = "Alexander Koval",
     author_email = "kovalidis@gmail.com",
--- solar/__init__.py
+++ solar/__init__.py
@@ -3,4 +3,4 @@ from query import SolrQuery, SolrError
 from util import X, LocalParams
 
 
-__version__ = (0, 2, 14, 'alpha')
+__version__ = (0, 2, 15, 'alpha')
