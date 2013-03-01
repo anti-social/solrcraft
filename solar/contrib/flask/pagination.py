401174bca3aaa5bc4bfd292a565ef6aa33ecd68b
Alexander Koval <kovalidis@gmail.com>
2013-03-01 19:41:37 +0200

    Added flask like pagination

---
+++ solar/contrib/flask/pagination.py
@@ -0,0 +1,134 @@
+# coding: utf-8
+from math import ceil
+
+class SolrQueryWrapper(object):
+    """Solr returns total count with response.
+    So we can get documents and count with one request.
+    """
+    def __init__(self, query, grouped_by=None):
+        self.query = query
+        self.grouped_by = grouped_by
+        self.sliced_query = None
+        self.items = None
+        self.count = None
+
+    def __getitem__(self, range):
+        if not isinstance(range, slice):
+            raise ValueError('__getitem__ without slicing not supported')
+        self.sliced_query = self.query[range]
+        if self.grouped_by:
+            grouped = self.sliced_query.results.get_grouped(self.grouped_by)
+            self.items = grouped.groups
+            self.count = grouped.ngroups
+        else:
+            self.items = list(self.sliced_query)
+            self.count = len(self.sliced_query)
+        return self.items
+
+    def __iter__(self):
+        if self.items is None:
+            raise ValueError('Slice first')
+        return iter(self.items)
+    
+    def __len__(self):
+        if self.count is None:
+            raise ValueError('Slice first')
+        return self.count
+
+    @property
+    def results(self):
+        if self.sliced_query is None:
+            raise ValueError('Slice first')
+        return self.sliced_query.results
+
+
+class Pagination(object):
+    """Helper class to provide compatibility with Flask-SQLAlchemy paginator.
+    """
+    def __init__(self, query, page=1, per_page=20):
+        self.original_query = query
+        if isinstance(query, SolrQueryWrapper):
+            self.query = query
+        else:
+            self.query = SolrQueryWrapper(query)
+        self.page = page if page > 0 else 1
+        self.per_page = per_page
+        self.offset = (self.page - 1) * self.per_page
+
+    def slice_query(self):
+        if self.query.count is None:
+            self.query[self.offset:self.offset + self.per_page]
+        
+    @property
+    def items(self):
+        self.slice_query()
+        return list(self.query)
+
+    @property
+    def total(self):
+        self.slice_query()
+        return len(self.query)
+
+    @property
+    def pages(self):
+        return int(ceil(self.total / float(self.per_page)))
+
+    def prev(self):
+        return type(self)(
+            self.original_query, page=self.prev_num, per_page=self.per_page)
+    
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def prev_num(self):
+        return self.page - 1
+
+    def next(self):
+        return type(self)(
+            self.original_query, page=self.next_num, per_page=self.per_page)
+
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    @property
+    def next_num(self):
+        return self.page + 1
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        """Iterates over the page numbers in the pagination. The four
+        parameters control the thresholds how many numbers should be produced
+        from the sides. Skipped page numbers are represented as `None`.
+        This is how you could render such a pagination in the templates:
+
+        .. sourcecode:: html+jinja
+
+        {% macro render_pagination(pagination, endpoint) %}
+          <div class=pagination>
+            {%- for page in pagination.iter_pages() %}
+              {% if page %}
+                {% if page != pagination.page %}
+                  <a href="{{ url_for(endpoint, page=page) }}">{{ page }}</a>
+                {% else %}
+                  <strong>{{ page }}</strong>
+                {% endif %}
+              {% else %}
+                <span class=ellipsis>…</span>
+              {% endif %}
+            {%- endfor %}
+          </div>
+        {% endmacro %}
+"""
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
---
+++ tests/test_flask_pagination.py
@@ -0,0 +1,130 @@
+from unittest import TestCase
+
+from mock import patch
+
+from solar import SolrSearcher
+from solar.contrib.flask.pagination import Pagination, SolrQueryWrapper
+
+
+class FlaskPaginationTest(TestCase):
+    def test_pagination(self):
+        s = SolrSearcher('http://example.com:8180/solr')
+        with patch.object(s.solrs_read[0], '_send_request') as send_request:
+            send_request.return_value = '''
+{
+  "response":{
+    "numFound":28,
+    "start":2,
+    "docs":[
+      {
+        "id":"333"
+      },
+      {
+        "id":"444"
+      }
+    ]
+  }
+}
+'''
+            p = Pagination(s.search(), page=2, per_page=2)
+
+            self.assertEqual(p.total, 28)
+            self.assertEqual(p.pages, 14)
+            self.assertEqual(len(p.items), 2)
+            self.assertEqual(p.items[0].id, '333')
+            self.assertEqual(p.items[1].id, '444')
+            self.assertEqual(p.has_next, True)
+            self.assertEqual(p.next_num, 3)
+            self.assertEqual(p.has_prev, True)
+            self.assertEqual(p.prev_num, 1)
+            for page, check_page in zip(p.iter_pages(), [1, 2, 3, 4, 5, 6, None, 13, 14]):
+                self.assertEqual(page, check_page)
+
+            self.assertEqual(send_request.call_count, 1)
+
+        with patch.object(s.solrs_read[0], '_send_request') as send_request:
+            send_request.return_value = '''
+{
+  "response":{
+    "numFound":18,
+    "start":20,
+    "docs":[]
+  }
+}
+'''
+            p = Pagination(s.search(), page=2)
+
+            self.assertEqual(p.total, 18)
+            self.assertEqual(p.pages, 1)
+            self.assertEqual(len(p.items), 0)
+            self.assertEqual(p.has_next, False)
+            self.assertEqual(p.next_num, 3)
+            self.assertEqual(p.has_prev, True)
+            self.assertEqual(p.prev_num, 1)
+
+            pp = p.prev()
+            self.assertEqual(pp.total, 18)
+            self.assertEqual(pp.pages, 1)
+
+            self.assertEqual(send_request.call_count, 2)
+
+    def test_grouped_pagination(self):
+        s = SolrSearcher('http://example.com:8180/solr')
+        with patch.object(s.solrs_read[0], '_send_request') as send_request:
+            send_request.return_value = '''
+{
+  "grouped":{
+    "company":{
+      "matches":281,
+      "ngroups":109,
+      "groups":[
+        {
+          "groupValue":"1",
+          "doclist":{"numFound":9,"start":0,"docs":[
+              {
+                "id":"111"
+              },
+              {
+                "id":"222"
+              },
+              {
+                "id":"333"
+              }
+            ]
+          }
+        },
+        {
+          "groupValue":"3",
+          "doclist":{"numFound":1,"start":0,"docs":[
+              {
+                "id":"555"
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
+'''
+            q = s.search().group('company', limit=3)
+            p = Pagination(SolrQueryWrapper(q, 'company'), page=29, per_page=2)
+
+            self.assertEqual(p.total, 109)
+            self.assertEqual(p.pages, 55)
+            self.assertEqual(len(p.items), 2)
+            self.assertEqual(p.items[0].value, '1')
+            self.assertEqual(p.items[0].ndocs, 9)
+            self.assertEqual(p.items[0].docs[0].id, '111')
+            self.assertEqual(p.items[0].docs[1].id, '222')
+            self.assertEqual(p.items[0].docs[2].id, '333')
+            self.assertEqual(p.items[1].value, '3')
+            self.assertEqual(p.items[1].ndocs, 1)
+            self.assertEqual(p.items[1].docs[0].id, '555')
+            self.assertEqual(p.has_next, True)
+            self.assertEqual(p.next_num, 30)
+            self.assertEqual(p.has_prev, True)
+            self.assertEqual(p.prev_num, 28)
+
+            self.assertEqual(send_request.call_count, 1)
+
