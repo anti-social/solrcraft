3af770f6970b37346f4d5efaf04f06b09af385e5
Alexander Koval <kovalidis@gmail.com>
2013-04-30 15:25:07 +0300

    #17 Error when stats is empty. Backported from 0.3

--- solar/stats.py
+++ solar/stats.py
@@ -1,12 +1,23 @@
 
+def maybe_float(v):
+    if v is not None:
+        return float(v)
+
+
+def maybe_int(v):
+    if v is not None:
+        return int(v)
+
+
 class Stats(object):
     def __init__(self, field, raw_stats):
+        raw_stats = raw_stats or {}
         self.field = field
-        self.min = raw_stats['min']
-        self.max = raw_stats['max']
-        self.sum = raw_stats['sum']
-        self.count = raw_stats['count']
-        self.missing = raw_stats['missing']
-        self.sum_of_squares = raw_stats['sumOfSquares']
-        self.mean = raw_stats['mean']
-        self.stddev = raw_stats['stddev']
+        self.min = maybe_float(raw_stats.get('min'))
+        self.max = maybe_float(raw_stats.get('max'))
+        self.sum = maybe_float(raw_stats.get('sum'))
+        self.count = maybe_int(raw_stats.get('count'))
+        self.missing = maybe_int(raw_stats.get('missing'))
+        self.sum_of_squares = maybe_float(raw_stats.get('sumOfSquares'))
+        self.mean = maybe_float(raw_stats.get('mean'))
+        self.stddev = maybe_float(raw_stats.get('stddev'))
