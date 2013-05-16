c2bfe0776e7ad46b2b9c35b37cae6fa57f80fc45
Alexander Koval <kovalidis@gmail.com>
2013-05-16 14:11:29 +0300

    Bumps version to 0.2.19

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requires():
 
 setup(
     name = "solar",
-    version = "0.2.18",
+    version = "0.2.19",
     requires = requires(),
     author = "Alexander Koval",
     author_email = "kovalidis@gmail.com",
